838c5d8ff18f51e8d8d105cef4f6e3d6fad26298
baixiaobest <baixiaohuang1@gmail.com>
2015-11-27 23:55:43 -0800

    fixed typo

--- Header.cpp
+++ Header.cpp
@@ -4,7 +4,7 @@
 #include <netinet/in.h>  // constants and structures needed for internet domain addresses, e.g. sockaddr_in
 #include <arpa/inet.h>
 #include <stdlib.h>
-#include <strings.h>
+#include <string.h>
 
 #include "Header.h"
 
@@ -30,9 +30,9 @@ void appendData(char* buffer, char* data, unsigned int dataLength){
 uint16_t calculateChecksum(char*buffer){
     header_t header;
     memcpy(&header, buffer, HEADERSIZE);
-    int originalCheckSum = header.checksum;
-    header.checksum = 0;
-    memcpy(buffer, header, HEADERSIZE);
+    int originalCheckSum = header.checkSum_m;
+    header.checkSum_m = 0;
+    memcpy(buffer, &header, HEADERSIZE);
 
     uint16_t dataLength = ((uint16_t)buffer[10]) << 8 | ((uint16_t)buffer[11] & 0xFF);
     uint32_t checksum = 0;
@@ -50,8 +50,8 @@ uint16_t calculateChecksum(char*buffer){
     }
     //one's complement
     checksum = ~checksum;
-    header.checksum = originalCheckSum;
-    memcpy(buffer, header, HEADERSIZE);
+    header.checkSum_m = originalCheckSum;
+    memcpy(buffer, &header, HEADERSIZE);
     return (uint16_t) (checksum & 0x0000FFFF);
 }
 
